f9d4d5dcd31e95dd58c6f2d7a30ba1973734249b
Stephen Crowe <crowe.stephen.m@gmail.com>
2019-09-30 22:57:31 -0400

    digits now works on the trained set

--- clothing.py
+++ clothing.py
@@ -16,6 +16,9 @@ fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images,
                                test_labels) = fashion_mnist.load_data()
 
+import sys
+
+
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
--- digits.py
+++ digits.py
@@ -9,18 +9,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import os
-
 import imageio
+import sys
 
 
-print(tf.__version__)
-
+images = []
+labels = []
 
 for filename in os.listdir("coil-20-proc"):
     filepath = "coil-20-proc/" + filename
     im = imageio.imread(filepath)
-    print(im.shape)
-    continue
+    images.append(im)
+    if filename[4] == '_':
+        labels.append(int(filename[3])-1)
+    else:
+        labels.append(int(filename[3:5])-1)
+
+class_names = []
+for i in range(20):
+    class_names.append("obj{0}".format(i))
+
+print(class_names)
+
+test_images = []
+test_labels = []
+train_images = []
+train_labels = []
+# Splice into training sets and test sets
+for i in range(len(images)):
+    if i % 10 == 5:
+        test_images.append(images[i])
+        test_labels.append(labels[i])
+    else:
+        train_images.append(images[i])
+        train_labels.append(labels[i])
+
+
 '''
 fashion_mnist = keras.datasets.fashion_mnist
 
@@ -29,17 +53,37 @@ fashion_mnist = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 '''
-'''
-train_images = train_images / 255.0
+
+
+def divideImageArray(imageArr):
+    for i in range(len(imageArr)):
+        for rowIdx in range(len(imageArr[i])):
+            for colIdx in range(len(imageArr[i][rowIdx])):
+                imageArr[i][rowIdx][colIdx] = imageArr[i][rowIdx][colIdx] / 255.0
+
+test_images = np.asarray(test_images)
+test_labels = np.asarray(test_labels)
+train_images = np.asarray(train_images)
+train_labels = np.asarray(train_labels)                
 
 test_images = test_images / 255.0
+train_images = train_images / 255.0
+
 
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Flatten(input_shape=(128, 128)), # Size of image is 128 x 128
     keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10, activation='softmax')
+    keras.layers.Dense(20, activation='softmax') # Number of classifiers
 ])
+
+'''
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+'''
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
@@ -76,9 +120,9 @@ def plot_image(i, predictions_array, true_label, img):
 def plot_value_array(i, predictions_array, true_label):
     predictions_array, true_label = predictions_array, true_label[i]
     plt.grid(False)
-    plt.xticks(range(10))
+    plt.xticks(range(20))
     plt.yticks([])
-    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    thisplot = plt.bar(range(20), predictions_array, color="#777777")
     plt.ylim([0, 1])
     predicted_label = np.argmax(predictions_array)
 
@@ -97,4 +141,4 @@ for i in range(num_images):
     plot_value_array(i, predictions[i], test_labels)
 plt.tight_layout()
 plt.show()
-'''
+
